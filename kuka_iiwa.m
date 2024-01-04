93057c73a85ab9e53aa44e39a67f703fc390826e
Go2SchooI <2354797131@qq.com>
2024-01-04 17:08:03 +0800

    use pinv for jacobian matrix

--- kuka_iiwa.m
+++ kuka_iiwa.m
@@ -2,7 +2,14 @@ close all,
 clear,clc;
 
 %% Modified DH parameters
-
+% 
+% L1 =  Link('revolute', 'd', 0.36, 'a', 0, 'aplpa', 0, 'modified');
+% L2 =  Link('revolute', 'd', 0, 'a', 0, 'aplpa', -pi/2, 'modified');
+% L3 =  Link('revolute', 'd', 0.42, 'a', 0, 'aplpa', pi/2, 'modified');
+% L4 =  Link('revolute', 'd', 0, 'a', 0, 'aplpa', -pi/2, 'modified');
+% L5 =  Link('revolute', 'd', 0.4, 'a', 0, 'aplpa', pi/2, 'modified');
+% L6 =  Link('revolute', 'd', 0, 'a', 0, 'aplpa', -pi/2, 'modified');
+% L7 =  Link('revolute', 'd', 0.126, 'a', 0, 'aplpa', pi/2, 'modified');
 L1 =  Link([ 0,    0.36,   0,       0,       0], 'modified');
 L2 =  Link([ 0,    0,      0,      -pi/2,    0], 'modified');
 L3 =  Link([ 0,    0.42,   0,       pi/2,    0], 'modified');
@@ -19,7 +26,6 @@ theta_target = [deg2rad(19.8), deg2rad(-33.6), deg2rad(-13.6), deg2rad(7.2), deg
 p_target = robot_modified.fkine(theta_target);
 q = rad2deg(robot_modified.ikine(p_target));
 
-%% 
 %% 正逆运动学均可调用自己编写库
 
 N = 400;
@@ -30,6 +36,9 @@ eplot = zeros(1,N);
 theta = zeros(1,robot_modified.n);
 
 use_Pseudo = 1;
+use_DLS = 1;
+lamda = 0.1;
+
 for j = 1 : N
     %平移    
     p = fkine(theta, robot_modified.n, robot_modified.alpha, robot_modified.a, robot_modified.d);
@@ -42,16 +51,22 @@ for j = 1 : N
     
     %旋转
     Rq = t2r(p);
-    rrr = t2r(p_target) * Rq';
     [thn,V] = tr2angvec(t2r(p_target) * Rq');
     e(4:6) = thn * V;
     eplot(j) = e * e';
    
 %     Jaco = jacob0(robot_modified, theta);
     Jaco = jacobe(theta, robot_modified.n, robot_modified.alpha, robot_modified.a, robot_modified.d);
+    JtJ = Jaco * Jaco';
+    
     if (use_Pseudo)
-%         dq = Jaco' * inv(Jaco * Jaco') * e';
-        dq = pinv(Jaco) * e';
+        if(use_DLS)
+            Jaco_dls = Jaco' * pinv(JtJ + lamda * lamda * eye(6));
+            dq = Jaco_dls * e';
+        else
+            Jaco_pinv = Jaco' * pinv(JtJ);
+            dq = Jaco_pinv * e';
+        end
     else
         dq = Jaco' * e';
     end
